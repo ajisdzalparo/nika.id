29278d436649f2b7f499d64907be5f781033c1da
Ajis Dzalparo <ajisdzalparo22@gmail.com>
2026-03-10 19:19:03 +0700

    remove component unused

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -4,8 +4,7 @@ import * as React from "react";
 import { IconTemplate, IconDashboard, IconMessage, IconUser, IconTransactionDollar } from "@tabler/icons-react";
 
 import { NavMain } from "@/components/nav-main";
-import { NavUser } from "@/components/nav-user";
-import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
+import { Sidebar, SidebarContent, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
 import Image from "next/image";
 import Link from "next/link";
 
